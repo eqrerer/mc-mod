6ebb8f0007d5ca80eaa941072c826c58d94592f6
Sidhvin Bysani <maccc@mac.lan>
2025-10-25 12:07:16 -0700

    Initial commit

---
+++ src/client/java/github/eqrerer/BloxtedStalkerClient.java
@@ -0,0 +1,10 @@
+package github.eqrerer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BloxtedStalkerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
